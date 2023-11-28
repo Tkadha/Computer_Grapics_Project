9dbf2ee343700512d73975f5e815b5f96ac8a709
Tkadha(home) <emforhs228@naver.com>
2023-11-29 00:18:38 +0900

    이걸로 시작

---
+++ CG_기말프로젝트.cpp
@@ -0,0 +1,327 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <gl/glew.h>
+#include <gl/freeglut.h>
+#include <gl/freeglut_ext.h>
+#include <gl/glm/ext.hpp>
+#include <gl/glm/glm.hpp>
+#include <gl/glm/ext/matrix_transform.hpp>
+#include <stdlib.h>
+#include <random>
+#include <math.h>
+#include <fstream>
+
+#define Width 1200
+#define Height 800
+#define side_length 0.2
+#define board_size 3
+#define PI 3.14159265
+#define snow_count 80
+void make_vertexShaders();
+void make_fragmentShaders();
+void make_shaderProgram();
+GLvoid drawScene();
+GLvoid Reshape(int w, int h);
+char* filetobuf(const char* file);
+void InitBuffer();
+GLvoid Keyboard(unsigned char key, int x, int y);
+GLchar* vertexSource, * fragmentSource; //--- 소스코드 저장 변수
+GLuint vertexShader, fragmentShader; //--- 세이더 객체
+GLuint shaderProgramID;
+GLuint vao;
+
+
+std::random_device rd;
+std::mt19937 gen(rd());
+std::uniform_real_distribution<> random_color(0.1, 1);
+
+class Shape {
+public:
+	GLuint vbo[3];
+	int vertex_count;
+	void load_Obj(const char* path)
+	{
+		int v_count = 0;
+		int n_count = 0;
+		int f_count = 0;
+		std::string lineHeader;
+		std::ifstream in(path);
+		if (!in) {
+			std::cerr << path << "파일 못찾음";
+			exit(1);
+		}
+		while (in >> lineHeader) {
+			if (lineHeader == "v")	++v_count;
+			else if (lineHeader == "f")	++f_count;
+		}
+		in.close();
+		in.open(path);
+
+		glm::vec3* vertex = new glm::vec3[v_count];
+		glm::vec3* face = new glm::vec3[f_count];
+		glm::vec3* vertexdata = new glm::vec3[f_count * 3];
+		glm::vec3* normaldata = new glm::vec3[f_count * 3];
+		glm::vec3* colordata = new glm::vec3[f_count * 3];
+		vertex_count = f_count * 3;
+		int v_incount = 0;
+		int f_incount = 0;
+		int color_count = 0;
+		while (in >> lineHeader) {
+			if (lineHeader == "v") {
+				in >> vertex[v_incount].x >> vertex[v_incount].y >> vertex[v_incount].z;
+				++v_incount;
+			}
+			else if (lineHeader == "f") {
+				in >> face[f_incount].x >> face[f_incount].y >> face[f_incount].z;
+				vertexdata[f_incount * 3 + 0] = vertex[static_cast<int>(face[f_incount].x - 1)];
+				vertexdata[f_incount * 3 + 1] = vertex[static_cast<int>(face[f_incount].y - 1)];
+				vertexdata[f_incount * 3 + 2] = vertex[static_cast<int>(face[f_incount].z - 1)];
+				++f_incount;
+			}
+		}
+		for (int i = 0; i < vertex_count / 3; i++)
+		{
+			glm::vec3 normal = glm::cross(vertexdata[i * 3 + 1] - vertexdata[i * 3 + 0], vertexdata[i * 3 + 2] - vertexdata[i * 3 + 0]);
+			normaldata[i * 3 + 0] = normal;
+			normaldata[i * 3 + 1] = normal;
+			normaldata[i * 3 + 2] = normal;
+
+		}
+		for (int i = 0; i < vertex_count; ++i) {
+			colordata[color_count].x = random_color(gen);
+			colordata[color_count].y = random_color(gen);
+			colordata[color_count].z = random_color(gen);
+			color_count++;
+		}
+
+		glGenBuffers(3, vbo);
+
+		glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+		glBufferData(GL_ARRAY_BUFFER, vertex_count * sizeof(glm::vec3), vertexdata, GL_STATIC_DRAW);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+		glEnableVertexAttribArray(0);
+
+		glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+		glBufferData(GL_ARRAY_BUFFER, vertex_count * sizeof(glm::vec3), colordata, GL_STATIC_DRAW);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
+		glEnableVertexAttribArray(1);
+
+		glBindBuffer(GL_ARRAY_BUFFER, vbo[2]);
+		glBufferData(GL_ARRAY_BUFFER, vertex_count * sizeof(glm::vec3), normaldata, GL_STATIC_DRAW);
+		glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 0, 0);
+		glEnableVertexAttribArray(2);
+
+		delete[] vertex;
+		vertex = nullptr;
+		delete[] face;
+		face = nullptr;
+		delete[] vertexdata;
+		vertexdata = nullptr;
+		delete[] normaldata;
+		normaldata = nullptr;
+		delete[] colordata;
+		colordata = nullptr;
+	}
+	void Set_color(float color) {
+		glm::vec3* colordata = new glm::vec3[vertex_count];
+		for (int i = 0; i < vertex_count; ++i) {
+			colordata[i].x = color;
+			colordata[i].y = color;
+			colordata[i].z = color;
+		}
+		glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+		glBufferData(GL_ARRAY_BUFFER, vertex_count * sizeof(glm::vec3), colordata, GL_STATIC_DRAW);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
+		glEnableVertexAttribArray(1);
+		delete[] colordata;
+		colordata = nullptr;
+	}
+};
+glm::vec3 light_pos;
+glm::vec3 light_color;
+void main(int argc, char** argv) //--- 윈도우 출력하고 콜백함수 설정
+{
+	//--- 윈도우 생성하기
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(Width, Height);
+	glutCreateWindow("Project_Portal");
+	glewExperimental = GL_TRUE;
+	glewInit();
+	make_shaderProgram();
+
+	{	//설명		
+		std::cout << "q/Q: 프로그램 종료" << std::endl;
+	}
+
+	{
+		light_pos.x = 1.f;
+		light_pos.y = 1.f;
+		light_pos.z = 2.f;
+		light_color.x = 1.0f;
+		light_color.y = 1.0f;
+		light_color.z = 1.0f;
+	}
+
+	InitBuffer();
+	//--- 세이더 읽어와서 세이더 프로그램 만들기
+	glutDisplayFunc(drawScene); //--- 출력 콜백 함수
+	glutReshapeFunc(Reshape);
+	glutKeyboardFunc(Keyboard);
+	glutMainLoop();
+}
+
+GLvoid drawScene() //--- 콜백 함수: 그리기 콜백 함수
+{
+	GLfloat rColor, gColor, bColor;
+	rColor = gColor = bColor = 0.0f;
+	glClearColor(rColor, gColor, bColor, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glUseProgram(shaderProgramID);
+	glBindVertexArray(vao);
+
+	glEnable(GL_DEPTH_TEST);
+	int PosLocation = glGetAttribLocation(shaderProgramID, "in_Position"); //	: 0
+	int ColorLocation = glGetAttribLocation(shaderProgramID, "in_Color"); //	: 1
+	int NormalLocation = glGetAttribLocation(shaderProgramID, "in_Normal"); //	: 2
+	glEnableVertexAttribArray(PosLocation);
+	glEnableVertexAttribArray(ColorLocation);
+	glEnableVertexAttribArray(NormalLocation);
+
+
+	unsigned int proj_location = glGetUniformLocation(shaderProgramID, "projection");
+	unsigned int view_location = glGetUniformLocation(shaderProgramID, "view");
+
+	//투영행렬
+	glm::mat4 proj = glm::mat4(1.0f);
+	proj = glm::perspective(glm::radians(45.0f), 1.0f, 0.1f, 50.0f);
+	proj = glm::translate(proj, glm::vec3(0.f, 0.f, -5.0f));
+	glUniformMatrix4fv(proj_location, 1, GL_FALSE, &proj[0][0]);
+
+	//뷰 행렬
+	glm::vec3 cameraPos = glm::vec3(0.f, 2.f, 2.0f); //--- 카메라 위치
+	glm::vec3 cameraDirection = glm::vec3(0.f, 0.f, 0.f); //--- 카메라 바라보는 방향
+	glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f); //--- 카메라 위쪽 방향
+	glm::mat4 view = glm::mat4(1.0f);
+
+	view = glm::lookAt(cameraPos, cameraDirection, cameraUp);
+	glUniformMatrix4fv(view_location, 1, GL_FALSE, &view[0][0]);
+
+
+	unsigned int lightPosLocation = glGetUniformLocation(shaderProgramID, "lightPos");
+	glUniform3f(lightPosLocation, light_pos.x, light_pos.y, light_pos.z);
+	unsigned int lightColorLocation = glGetUniformLocation(shaderProgramID, "lightColor");
+	glUniform3f(lightColorLocation, light_color.x, light_color.y, light_color.z);
+	unsigned int viewPosLocation = glGetUniformLocation(shaderProgramID, "viewPos");
+	glUniform3f(viewPosLocation, cameraPos.x, cameraPos.y, cameraPos.z);
+	unsigned int AmbientPosLocation = glGetUniformLocation(shaderProgramID, "amb_light");
+
+
+	glm::mat4 trans = glm::mat4(1.0f);
+	unsigned int shape_location = glGetUniformLocation(shaderProgramID, "transform");
+	glUniformMatrix4fv(shape_location, 1, GL_FALSE, glm::value_ptr(trans));
+
+
+
+	glDisableVertexAttribArray(ColorLocation);
+	glDisableVertexAttribArray(PosLocation);
+	glDisableVertexAttribArray(NormalLocation);
+
+
+	glutSwapBuffers(); // 화면에 출력하기
+}
+GLvoid Reshape(int w, int h) //--- 콜백 함수: 다시 그리기 콜백 함수
+{
+	glViewport(0, 0, w, h);
+}
+void make_vertexShaders()
+{
+	vertexSource = filetobuf("vertex_project.glsl");
+	//--- 버텍스 세이더 객체 만들기
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	//--- 세이더 코드를 세이더 객체에 넣기
+	glShaderSource(vertexShader, 1, (const GLchar**)&vertexSource, 0);
+	//--- 버텍스 세이더 컴파일하기
+	glCompileShader(vertexShader);
+	//--- 컴파일이 제대로 되지 않은 경우: 에러 체크
+	GLint result;
+	GLchar errorLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &result);
+	if (!result)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, errorLog);
+		std::cout << "ERROR: vertex shader 컴파일 실패\n" << errorLog << std::endl;
+		return;
+	}
+}
+void make_fragmentShaders()
+{
+	fragmentSource = filetobuf("fragment_project.glsl");
+	//--- 프래그먼트 세이더 객체 만들기
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	//--- 세이더 코드를 세이더 객체에 넣기
+	glShaderSource(fragmentShader, 1, (const GLchar**)&fragmentSource, 0);
+	//--- 프래그먼트 세이더 컴파일
+	glCompileShader(fragmentShader);
+	//--- 컴파일이 제대로 되지 않은 경우: 컴파일 에러 체크
+	GLint result;
+	GLchar errorLog[512];
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &result);
+	if (!result)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, errorLog);
+		std::cerr << "ERROR: fragment shader 컴파일 실패\n" << errorLog << std::endl;
+		return;
+	}
+}
+void make_shaderProgram()
+{
+	make_vertexShaders(); //--- 버텍스 세이더 만들기
+	make_fragmentShaders(); //--- 프래그먼트 세이더 만들기
+	//-- shader Program
+	shaderProgramID = glCreateProgram();
+	glAttachShader(shaderProgramID, vertexShader);
+	glAttachShader(shaderProgramID, fragmentShader);
+	glLinkProgram(shaderProgramID);
+	//--- 세이더 삭제하기
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+	//--- Shader Program 사용하기
+	glUseProgram(shaderProgramID);
+}
+char* filetobuf(const char* file)
+{
+	FILE* fptr;
+	long length;
+	char* buf;
+	fptr = fopen(file, "rb"); // Open file for reading
+	if (!fptr) // Return NULL on failure
+		return NULL;
+	fseek(fptr, 0, SEEK_END); // Seek to the end of the file
+	length = ftell(fptr); // Find out how many bytes into the file we are
+	buf = (char*)malloc(length + 1); // Allocate a buffer for the entire length of the file and a null terminator
+	fseek(fptr, 0, SEEK_SET); // Go back to the beginning of the file
+	fread(buf, length, 1, fptr); // Read the contents of the file in to the buffer
+	fclose(fptr); // Close the file
+	buf[length] = 0; // Null terminator
+	return buf; // Return the buffer
+}
+void InitBuffer() {
+
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+
+}
+GLvoid Keyboard(unsigned char key, int x, int y)
+{
+	switch (key) {
+	case 'q':
+	case 'Q':
+		exit(0);
+		break;
+	}
+	glutPostRedisplay();
+}
